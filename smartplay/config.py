678b7f3c0e894584922d65df50b3ec670be3caf1
Eric Ho <wingsum.developer@gmail.com>
2025-06-21 07:43:52 +0800

    add setting

--- smartplay/config.py
+++ smartplay/config.py
@@ -5,3 +5,5 @@ class Config:
    PREFER_NIGHT = True  # 偏好夜晚時段
    MUST_BOOK_2_HOUR = False
    START_DATE = "2025-06-26"  # 預設開始日期
+   QUEUE_REMINDER = True  # 是否發音排隊狀態
+   QUEUE_REMINDER_THRESHOLD = 4000  # 排隊人數小過此數量才提醒
